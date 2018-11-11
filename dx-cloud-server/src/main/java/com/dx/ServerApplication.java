c30d4690fc683f02b1d680a951a0763c09a9bdde
670139644@qq.com <670139644@qq.com>
2018-11-11 14:19:36 +0800

    初始化项目

---
+++ dx-cloud-server/src/main/java/com/dx/ServerApplication.java
@@ -0,0 +1,21 @@
+package com.dx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * dx-cloud
+ *
+ * @author: yaoj
+ * @Date: 2018/11/11 14:07
+ * 版权所有：Copyright 2018 by 文理电信
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
