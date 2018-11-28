65a13c1c09cec94467994eaad198a40f5e9fd43f
670139644@qq.com <670139644@qq.com>
2018-11-29 00:46:42 +0800

    zuul网切换成gateway网关组件

---
+++ dx-cloud-gateway/src/main/java/com/dx/GatewayApplication.java
@@ -0,0 +1,35 @@
+package com.dx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Description:
+ *
+ * @author yaoj
+ * @version 1.0
+ * @copyright Copyright (c) 文理电信
+ * @since 2018-11-13
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //basic proxy
+                .route(r -> r.path("/baidu")
+                        .uri("http://baidu.com:80/")
+                ).build();
+    }
+
+}
