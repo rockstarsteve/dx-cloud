756c618999e398172009dbdbbde32f52ae7f69c9
姚俭 <yaoj@minstone.com.cn>
2018-12-06 08:42:15 +0800

    修改gateway模块

--- dx-cloud-gateway/src/main/java/com/dx/GatewayApplication.java
+++ dx-cloud-gateway/src/main/java/com/dx/GatewayApplication.java
@@ -25,11 +25,25 @@ public class GatewayApplication {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        String httpUri = "http://httpbin.org:80";
         return builder.routes()
-                //basic proxy
-                .route(r -> r.path("/baidu")
-                        .uri("http://baidu.com:80/")
-                ).build();
+
+                //断言/get请求
+                .route(r -> r.path("/get")
+                        //该filter会将请求添加一个header,key为hello，value为world
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+
+                //host有“*.hystrix.com”，都会进入该router，该router中有一个hystrix的filter,该filter可以配置名称、和指向性fallback的逻辑的地址
+                //
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
     }
 
 }
