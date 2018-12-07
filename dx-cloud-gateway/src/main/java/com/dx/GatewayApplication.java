f5d75cf7f579579f0cfac3f535aa1d69d40b4be4
姚俭 <yaoj@minstone.com.cn>
2018-12-07 18:50:42 +0800

    ouath认证模块修改

--- dx-cloud-gateway/src/main/java/com/dx/GatewayApplication.java
+++ dx-cloud-gateway/src/main/java/com/dx/GatewayApplication.java
@@ -2,13 +2,9 @@ package com.dx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Description:
@@ -18,10 +14,10 @@ import org.springframework.context.annotation.Bean;
  * @copyright Copyright (c) 文理电信
  * @since 2018-11-13
  */
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 @EnableCircuitBreaker
-@EnableFeignClients(basePackages = "com.dx.auth.client")
+@EnableFeignClients//(basePackages = "com.dx.auth.client")//(basePackages = "com.dx.auth.client")
+@SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
